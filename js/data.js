3913727a6b609b9311132a8e9bd837cf77763439
Erikas J <erikas.privat3043@gmail.com>
2025-03-19 04:00:40 +0100

    lyrics

--- js/data.js
+++ js/data.js
@@ -12,18 +12,98 @@ window.AppData = [
         "artist": "Black Pumas",
         "year": "2019",
         "links": [
-            "https://music.youtube.com/watch?v=wyDZyclIzFo&si=HDfDSL3DEKDR7QMb"
+            {
+                "service": "Spotify",
+                "url": "https://open.spotify.com/track/6J3HNhDBIwEX11bX8hzx5u?si=6c74adf4c8234535"
+            },
+            {
+                "service": "YT Music",
+                "url": "https://music.youtube.com/watch?v=wyDZyclIzFo&si=LgNPnFAjg6Q_hht8"
+            }
         ],
-        "lyrics": "I woke up to the morning sky first"
-    },
-    {
-        "img": "colors-black-pumas",
-        "name": "Colors",
-        "artist": "Black Pumas",
-        "year": "2019",
-        "links": [
-            "https://music.youtube.com/watch?v=wyDZyclIzFo&si=HDfDSL3DEKDR7QMb"
-        ],
-        "lyrics": "I woke up to the morning sky first"
+        "lyrics": [
+            {
+                type: "verse",
+                number: 1,
+                lines: [
+                    "I woke up to the morning sky first",
+                    "Baby blue, just like we rehearsed",
+                    "When I get up off this ground, I shake leaves back down",
+                    "To the brown, brown, brown, brown 'til I'm clean",
+                    "Then I walked where I'd be shaded by the trees",
+                    "By a meadow of green",
+                    "For about a mile",
+                    "I'm headed to town, town, town in style"
+                ]
+            },
+            {
+                type: "chorus",
+                lines: [
+                    "With all my favorite colors (yes, sir)",
+                    "All my favorite colors (right on)",
+                    "My sisters and my brothers see 'em like no other",
+                    "All my favorite colors"
+                ]
+            },
+            {
+                type: "verse",
+                number: 2,
+                lines: [
+                    "It's a-good day to be, a good day for me",
+                    "A good day to see my favorite colors, colors",
+                    "My sisters and my brothers, they see 'em like no other",
+                    "All my favorite colors"
+                ]
+            },
+            {
+                type: "chorus",
+                lines: [
+                    "With all my favorite colors (yes sir)",
+                    "All my favorite colors (yes ma'am)",
+                    "My sisters and my brothers see 'em like no other",
+                    "All my favorite colors"
+                ]
+            },
+            {
+                type: "bridge",
+                lines: [
+                    "Now take me to the other side",
+                    "Where the baby blues birds fly",
+                    "In grey clouds, or white walls",
+                    "Or blue skies we gon' fly, feel alright",
+                    "Now we gon' ooh-ooh, ooh-ooh, ooh-ooh, ooh, yeah",
+                    "Each time like ooh-ooh, ooh-ooh, ooh-ooh, ooh, yeah",
+                    "And at least I can say I anticipate",
+                    "A homecome parade as we renegade in the morning light"
+                ]
+            },
+            {
+                type: "chorus",
+                lines: [
+                    "With all my favorite colors (yes sir)",
+                    "All my favorite colors (yes ma'am)",
+                    "My sisters and my brothers see 'em like no other",
+                    "All my favorite colors"
+                ]
+            },
+            {
+                type: "chorus",
+                lines: [
+                    "All my favorite colors (right)",
+                    "All my favorite colors (yes ma'am)",
+                    "My sisters and my brothers see 'em like no other",
+                    "All my favorite colors"
+                ]
+            },
+            {
+                type: "outro",
+                lines: [
+                    "It's good day to be, a good day for me",
+                    "A good day to see my favorite colors, colors",
+                    "My sisters and my brothers, they see 'em like no other",
+                    "All my favorite colors"
+                ]
+            }
+        ]
     }
 ]
--- js/views/SongView.js
+++ js/views/SongView.js
@@ -14,25 +14,56 @@ class SongView extends Component {
 
         return `
             <div class="container mainSongContainer">
-                <header class="wrapper">
+                <header class="wrapper gapMedium">
                     <img id="headerImg" src="/assets/${song.img}.jpg" alt="${song.name}">
 
-                    <div class="container headerBody">
-                        <div class="container">
-                            <h3>${song.name}</h3>
-                            <div class="wrapper">
-                                <p>${song.artist}</p>
-                                <p>${song.year}</p>
+                    <div class="container gapLarge headerBody">
+                        <div class="container gapSmall">
+                            <h2 class="title">${song.name}</h2>
+                            <div class="wrapper gapSmall">
+                                <p class="footnote">${song.artist}</p>
+                                <p class="footnote">${song.year}</p>
                             </div>
                         </div>
 
-                        <div class="wrapper">
-                            <a href="">Music app</a>
+                        <div class="wrapper gapSmall musicAppsWrapper">
+                            ${song.links.map(link =>`
+                                <a class="musicAppLinkBtn regular" href="${link.url}" target="_blank">${link.service}</a>
+                            `).join("")}
                         </div>
                     </div>
                 </header>
+
+                ${song.lyrics.map(lyric =>`
+                    <div class="container gapSmall lyricContainer">
+                        <h3 class="lyricType regular">${lyric.type}</h3>
+
+                        ${lyric.lines.map(line =>`
+                            <p class="lyric regular">${line}</p>
+                        `).join("")}
+                    </div>
+                `).join("")}
             </div>
         `
+    }
+
+    setup() {
+        // Get references to the header and headerBody elements
+        const header = this.element.querySelector('header');
+        const headerBody = this.element.querySelector('.headerBody');
+
+        // Function to update the header height
+        const updateHeaderHeight = () => {
+            const headerBodyHeight = headerBody.offsetHeight;
+            header.style.height = `${headerBodyHeight}px`;
+        };
+
+        // Initial call to set the height
+        updateHeaderHeight();
+
+        // Optionally, you can add a resize observer to handle dynamic changes
+        const resizeObserver = new ResizeObserver(updateHeaderHeight);
+        resizeObserver.observe(headerBody);
     }
 }
 
